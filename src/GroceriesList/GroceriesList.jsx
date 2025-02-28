78267181ce75632b511a685d77a30f94d6046e87
Atif-Mustafa <91708857+Atif-Mustafa@users.noreply.github.com>
2025-02-28 14:09:54 +0530

    Update GroceriesList.jsx

--- src/GroceriesList/GroceriesList.jsx
+++ src/GroceriesList/GroceriesList.jsx
@@ -1,25 +1,3 @@
-// import GroceryItem from "../GroceryItem/GroceryItem"
-// const GroceriesList = (props) => {
-//     return (
-//         <>
-//         {
-//             props.groceriesList.map((grocery, index) =>{
-//                return  <GroceryItem 
-//                grocery={grocery} 
-//                key={index}
-//                index = {index}
-//                removeGrocery={props.removeGrocery}
-//                toggleGrocery={props.toggleGrocery}/>
-//             })
-//         }
-//         </>
-//     )
-// }
-
-// export default GroceriesList
-
-
-
 import GroceryItem from "../GroceryItem/GroceryItem"
 
 const GroceriesList = (props) => {
